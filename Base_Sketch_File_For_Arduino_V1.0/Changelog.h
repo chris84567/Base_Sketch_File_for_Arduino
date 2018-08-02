5752dc4ad6f5feae3b330ececf119db304ba3b5f
chris84567 <41809128+chris84567@users.noreply.github.com>
2018-08-01 23:32:14 -0400

    Uploaded V1.0

---
+++ Base_Sketch_File_For_Arduino_V1.0/Changelog.h
@@ -0,0 +1,13 @@
+/*
+ * 
+ * Base Sketch File for Arduino
+ * 
+ * By: Joe Conway
+ * 
+ * Published: 08/01/2018
+ * 
+ * Github Link: https://github.com/chris84567/Base_Sketch_File_for_Arduino
+ *  
+*/
+
+
